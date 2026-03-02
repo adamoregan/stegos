e493c85df937706ab306a1f95c3138704783d27c
adamoregan <adamoregan457@gmail.com>
2026-03-02 16:38:44 +0000

    Add Argon2 key derivation decorator

--- stegos/steganography/decorators/encryption.py
+++ stegos/steganography/decorators/encryption.py
@@ -1,20 +1,68 @@
+import base64
+import os
+from typing import Callable, TypeAlias
+
 import numpy as np
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.kdf import KeyDerivationFunction
+from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
 from stegos.steganography.decorators.decorator import BaseLSBSteganographyDecorator
 
 
-class EncryptionDecorator(BaseLSBSteganographyDecorator):
-    """Encrypts the payload before embedding."""
+def _default_argon2(salt: bytes) -> Argon2id:
+    """
+    Provides a default key derivation function.
+    :param salt: Salt used for key derivation.
+    :return: Argon2id key derivation function.
+    """
+    # See rfc9106/section-4
+    return Argon2id(
+        salt=salt,
+        length=32,
+        iterations=1,
+        lanes=4,
+        memory_cost=2**21,
+        ad=None,
+        secret=None,
+    )
 
-    def __init__(self, strategy, key: bytes):
+
+KDF: TypeAlias = Callable[[bytes], KeyDerivationFunction]
+
+
+class EncryptionDecorator(BaseLSBSteganographyDecorator):
+    """Encrypts the payload before embedding.
+
+    The encryption key is derived from the given password. The salt used for key derivation is embedded in the image.
+    """
+
+    SALT_LENGTH = 16
+
+    def __init__(self, strategy, password: bytes, kdf: KDF = None):
         super().__init__(strategy)
-        self._fernet = Fernet(key)
+        self._password = password
+        self._kdf = kdf or _default_argon2
+
+    def _derive_key(self, salt: bytes) -> bytes:
+        """
+        Derives a key for encryption.
+        :param salt: Salt used for key derivation.
+        :return: Base64 encoded key.
+        """
+        key = self._kdf(salt).derive(self._password)
+        return base64.urlsafe_b64encode(key)
 
     def embed(self, cover_image: np.ndarray, payload: bytes) -> np.ndarray:
-        encrypted_payload = self._fernet.encrypt(payload)
-        return self.strategy.embed(cover_image, encrypted_payload)
+        salt = os.urandom(self.SALT_LENGTH)
+        key = self._derive_key(salt)
+        payload = salt + Fernet(key).encrypt(payload)
+        return self.strategy.embed(cover_image, payload)
 
     def extract(self, stego_image: np.ndarray) -> bytes:
-        extracted = super().extract(stego_image)
-        return self._fernet.decrypt(extracted)
+        payload = super().extract(stego_image)
+        salt, encrypted_payload = (
+            payload[: self.SALT_LENGTH],
+            payload[self.SALT_LENGTH :],
+        )
+        return Fernet(self._derive_key(salt)).decrypt(encrypted_payload)
--- tests/unit/steganography/decorators/test_encryption.py
+++ tests/unit/steganography/decorators/test_encryption.py
@@ -1,13 +1,31 @@
 import pytest
-from cryptography.fernet import Fernet, InvalidToken
+from cryptography.fernet import InvalidToken
+from cryptography.hazmat.primitives.kdf.argon2 import Argon2id
 
 from stegos.steganography.decorators.encryption import EncryptionDecorator
 from tests.unit.steganography.util import create_image, Dummy
 
 
+def _lightweight_argon2(salt: bytes) -> Argon2id:
+    """
+    Provides a default key derivation function for testing purposes.
+    :param salt: Salt used for key derivation.
+    :return: Argon2id key derivation function.
+    """
+    return Argon2id(
+        salt=salt,
+        length=32,
+        iterations=1,
+        lanes=1,
+        memory_cost=1 * 8,
+        ad=None,
+        secret=None,
+    )
+
+
 @pytest.fixture
-def steg():
-    return EncryptionDecorator(Dummy(), Fernet.generate_key())
+def steg() -> EncryptionDecorator:
+    return EncryptionDecorator(Dummy(), b"password", _lightweight_argon2)
 
 
 class TestEncryptionDecorator:
@@ -20,10 +38,20 @@ class TestEncryptionDecorator:
         assert steg.strategy._payload != payload
         assert steg.extract(embedded) == payload
 
+    def test_embed_extract_uniqueness(self, steg):
+        """Embedding with the same password should not result in the same ciphertext."""
+        payload = b"Embedded Payload"
+        cover_image = create_image()
+        steg.embed(cover_image, payload)
+        ciphertext1 = steg.strategy._payload
+        steg.embed(cover_image, payload)
+        ciphertext2 = steg.strategy._payload
+        assert ciphertext1 != ciphertext2
+
     def test_key_failure(self, steg):
         """Decryption should be key-dependent."""
         payload = b"Embedded Payload"
         embedded = steg.embed(create_image(), payload)
-        steg2 = EncryptionDecorator(steg.strategy, Fernet.generate_key())
+        steg2 = EncryptionDecorator(steg.strategy, b"wrong_password")
         with pytest.raises(InvalidToken):
             steg2.extract(embedded)
